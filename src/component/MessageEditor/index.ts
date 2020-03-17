694b55b9d64028142dc8ad1f2c6beb24fb41f3cc
Hook <hookhookun@gmail.com>
2020-03-17 14:18:04 +0900

    Display message information

--- src/component/MessageEditor/index.ts
+++ src/component/MessageEditor/index.ts
@@ -4,20 +4,25 @@ import {MessageId} from '../../types';
 import {MessagePreview} from '../MessagePreview';
 import {useSelector} from 'react-redux';
 import {MessageControl} from '../MessageControl';
-import className from './style.css';
 import {MessageTextEditor} from '../MessageTextEditor';
 import {selectCurrentMessageIdList} from '../../core/selector';
 import {MessageTextTool} from '../MessageTextTool';
+import className from './style.css';
 
 export const MessageEditor = ({id}: {id: MessageId}): ReactElement => {
     const activeMessageIdList = useSelector(selectCurrentMessageIdList);
+    const active = activeMessageIdList.includes(id);
     return createElement(
         'div',
         {
             id: `Editor-${id}`,
+            'data-title': [
+                `${id}`,
+                active ? '（表示中）' : '',
+            ].join(' '),
             className: classnames(
                 className.container,
-                activeMessageIdList.includes(id) && className.current,
+                active && className.current,
             ),
         },
         createElement(MessagePreview, {id}),
